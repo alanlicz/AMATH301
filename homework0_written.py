32be3b3be824414ce60f9f2814b14a101fe95175
Alan Li <alanli@Alans-MacBook-Pro.local>
2021-01-06 12:18:32 -0800

    initial commit

---
+++ homework0_written.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(-2, 2.25, 0.25)
+y = x**2 - 1
+plt.figure()
+plt.plot(x, y, 'k')
+plt.plot(x, y, 'bo')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Plot of a parabola')
+plt.show()
